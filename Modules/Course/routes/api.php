fbf8be200796fff3464d738cbb846dde1980b54f
pepypeppep <pepy.peppep@gmail.com>
2025-01-25 11:34:13 +0700

    Implement course level and category api

--- Modules/Course/routes/api.php
+++ Modules/Course/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\Course\app\Http\Controllers\Api\CourseController;
+use Modules\Course\app\Http\Controllers\Api\CourseReviewController;
+use Modules\Course\app\Http\Controllers\Api\CourseCategoryController;
+use Modules\Course\app\Http\Controllers\Api\CourseLevelController;
 
 /*
     |--------------------------------------------------------------------------
@@ -15,5 +19,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function () {
-    Route::get('course', fn (Request $request) => $request->user())->name('course');
+    Route::get('course', fn(Request $request) => $request->user())->name('course');
+});
+
+
+Route::name('api.')->group(function () {
+    Route::get('course/{slug}/file', [CourseController::class, 'file'])->name('course.file');
+    Route::resource('course', CourseController::class)->names('course');
+    Route::resource('course-reviews', CourseReviewController::class)->names('course.reviews');
+    Route::resource('course-category', CourseCategoryController::class)->names('course.category');
+    Route::resource('course-level', CourseLevelController::class)->names('course.level');
 });

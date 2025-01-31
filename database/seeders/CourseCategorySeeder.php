2077f62b4254fcc308edf6da281cd6976a2f54bf
Dicky Arisya Rizaldi Ramadandi <dickyarisya88@gmail.com>
2025-01-31 15:19:52 +0700

    update course category seeder

--- database/seeders/CourseCategorySeeder.php
+++ database/seeders/CourseCategorySeeder.php
@@ -2,33 +2,117 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Http\Request;
 use Illuminate\Database\Seeder;
 use Modules\Course\app\Models\CourseCategory;
+use Modules\Course\app\Models\CourseCategoryTranslation;
+use Modules\Language\app\Enums\TranslationModels;
+use Modules\Language\app\Traits\GenerateTranslationTrait;
 
 class CourseCategorySeeder extends Seeder
 {
+    use GenerateTranslationTrait;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $category = CourseCategory::create([
-            'slug' => 'IT DEPARTMENT',
+        $request = new Request([
+            'name' => 'IT DEPARTMENT',
+            'slug' => 'it-department',
         ]);
-
-        CourseCategory::create([
-            'slug' => 'SOFTWARE ENGINEERING',
-            'parent_id' => $category->id
-        ]);
-
 
         $category = CourseCategory::create([
-            'slug' => 'MANAGEMENT DEPARTMENT',
+            'slug' => $request->slug,
+            'icon' => 'uploads/custom-images/it-department.png',
+            'status' => 1
         ]);
 
-        CourseCategory::create([
-            'slug' => 'MARKETING',
-            'parent_id' => $category->id
+        CourseCategoryTranslation::create([
+            'name' => $request->name,
+            'course_category_id' => $category->id,
+            'lang_code' => 'id'
         ]);
+
+        // $this->generateTranslations(
+        //     TranslationModels::CourseCategory,
+        //     $category,
+        //     'course_category_id',
+        //     $request,
+        // );
+
+        $request = new Request([
+            'name' => 'SOFTWARE ENGINEERING',
+            'slug' => 'software-engineering',
+        ]);
+
+        $category = CourseCategory::create([
+            'slug' => $request->slug,
+            'parent_id' => $category->id,
+            'icon' => 'uploads/custom-images/it-department.png',
+            'status' => 1
+        ]);
+
+        CourseCategoryTranslation::create([
+            'name' => $request->name,
+            'course_category_id' => $category->id,
+            'lang_code' => 'id'
+        ]);
+        // $this->generateTranslations(
+        //     TranslationModels::CourseCategory,
+        //     $category,
+        //     'course_category_id',
+        //     $request,
+        // );
+
+        $request = new Request([
+            'name' => 'MANAGEMENT DEPARTMENT',
+            'slug' => 'management-department',
+        ]);
+
+        $category =  CourseCategory::create([
+            'slug' => $request->slug,
+            'icon' => 'uploads/custom-images/it-department.png',
+            'status' => 1
+        ]);
+
+        CourseCategoryTranslation::create([
+            'name' => $request->name,
+            'course_category_id' => $category->id,
+            'lang_code' => 'id'
+        ]);
+
+        // $this->generateTranslations(
+        //     TranslationModels::CourseCategory,
+        //     $category,
+        //     'course_category_id',
+        //     $request,
+        // );
+
+
+        $request = new Request([
+            'name' => 'MARKETING',
+            'slug' => 'marketing',
+        ]);
+
+        $category =  CourseCategory::create([
+            'slug' => $request->slug,
+            'parent_id' => $category->id,
+            'icon' => 'uploads/custom-images/it-department.png',
+            'status' => 1
+        ]);
+
+        CourseCategoryTranslation::create([
+            'name' => $request->name,
+            'course_category_id' => $category->id,
+            'lang_code' => 'id'
+        ]);
+
+        // $this->generateTranslations(
+        //     TranslationModels::CourseCategory,
+        //     $category,
+        //     'course_category_id',
+        //     $request,
+        // );
     }
 }

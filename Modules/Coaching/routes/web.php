ecaa74d05edca703340a4a4f3eb0de2e6b447662
pepypeppep <pepy.peppep@gmail.com>
2025-06-24 08:21:14 +0700

    Fix coaching

--- Modules/Coaching/routes/web.php
+++ Modules/Coaching/routes/web.php
@@ -14,6 +14,33 @@ use Modules\Coaching\app\Http\Controllers\CoachingController;
 |
 */
 
-Route::group([], function () {
-    Route::resource('coaching', CoachingController::class)->names('coaching');
+Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'student', 'as' => 'student.'], function () {
+    Route::prefix('/coachee')->name('coachee.')->group(function () {
+        Route::get('/', function () {
+            return 'Coachee';
+        })->name('index');
+        // Route::get('/', [MenteeController::class, 'index'])->name('index');
+        // Route::get('/create', [MenteeController::class, 'create'])->name('create');
+        // Route::post('/', [MenteeController::class, 'store'])->name('store');
+        // Route::put('/session/update', [MenteeController::class, 'updateSession'])->name('update.session');
+        // Route::put('/{mentoring}/report', [MenteeController::class, 'updateFinalReport'])->name('report');
+        // Route::get('/{id}', [MenteeController::class, 'show'])->name('show');
+        // Route::get('/{id}/document/{type}', [MenteeController::class, 'showDocument'])->name('view.document');
+        // Route::get('/{id}/img', [MenteeController::class, 'viewImage'])->name('view.img');
+        // Route::put('/{id}/submit', [MenteeController::class, 'submitForApproval'])->name('submit');
+    });
+
+    Route::group(['prefix' => 'coach', 'as' => 'coach.'], function () {
+        Route::get('/', function () {
+            return 'Coach';
+        })->name('index');
+        // Route::get('/', [MentorController::class, 'index'])->name('index');
+        // Route::get('{id}/show', [MentorController::class, 'show'])->name('show');
+        // Route::post('{id}/approve', [MentorController::class, 'approve'])->name('approve');
+        // Route::post('{id}/reject', [MentorController::class, 'reject'])->name('reject');
+        // Route::post('{id}/review', [MentorController::class, 'review'])->name('review');
+        // Route::get('{id}/evaluasi', [MentorController::class, 'evaluasi'])->name('evaluasi');
+        // Route::post('{id}/evaluasi', [MentorController::class, 'evaluasiStore'])->name('evaluasi.store');
+        // Route::post('{id}/kirim-evaluasi', [MentorController::class, 'kirimEvaluasi'])->name('evaluasi.kirim');
+    });
 });

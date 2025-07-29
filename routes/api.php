d90e7f9c07f0dc7e411d14b1119f7d679c2be23e
pepypeppep <pepy.peppep@gmail.com>
2025-07-29 08:59:03 +0700

    Implement KMS Update and Edit API

--- routes/api.php
+++ routes/api.php
@@ -97,6 +97,8 @@ Route::name('api.')->group(function () {
         Route::group(['middleware' => ['auth:sso-api']], function () {
             Route::post('/articles', [ArticleController::class, 'store'])->name('store');
             Route::post('/articles/{slug}/submit', [ArticleController::class, 'submit'])->name('submit');
+            Route::get('/articles/{slug}/edit', [ArticleController::class, 'edit'])->name('edit');
+            Route::post('/articles/{slug}/update', [ArticleController::class, 'update'])->name('update');
             Route::post('/articles-reviews/{id}', [ArticleController::class, 'storeReviews'])->name('reviews.store');
         });
     });
